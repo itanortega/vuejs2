894925d2a6387dc43aa3029c576e638605b3c32e
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-01-12 09:32:43 -0500

    rutas con router-link

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .card
+  .card(v-if="track && track.album")
     .card-image
       figure.image.is-1by1
         img(:src="track.album.images[0].url")
@@ -18,6 +18,8 @@
           .level-left
             a.level-item
               span.icon.is-small(@click="selectTrack") Play
+            a.level-item
+              span.icon.is-small(@click="goToTrack(track.id)") Detalle
 </template>
 
 <script>
@@ -29,6 +31,9 @@ export default {
     selectTrack () {
       this.$emit('select', this.track.id)
       this.$bus.$emit('set-track', this.track)
+    },
+    goToTrack (id) {
+      this.$router.push({ name: 'track', params: { id } })
     }
   }
 }
---
+++ src/components/TrackDetail.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .container
+    .columns
+      .column.is-5.is-offset-4
+        pm-track(:track="track")
+</template>
+
+<script>
+import TrackService from '@/services/track'
+import PmTrack from '@/components/Track.vue'
+
+export default {
+  components: {
+    PmTrack
+  },
+  data () {
+    return {
+      track: {}
+    }
+  },
+  created () {
+    const id = this.$route.params.id
+
+    TrackService.getById(id)
+      .then(res => {
+        this.track = res
+      })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .columns {
+    margin: 20px;
+  }
+</style>
